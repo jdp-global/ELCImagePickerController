5b49ec50a8c0c71d3ddf9b61c9880a1e6454ee7f
Dominick Critelli <dominick@purplepenguin.com>
2013-05-08 15:16:51 -0400

    Add method to convert asset url to a .MOV file in the temp directory

--- Classes/ELCImagePicker/ELCImagePickerController.m
+++ Classes/ELCImagePicker/ELCImagePickerController.m
@@ -51,6 +51,66 @@
     }
 }
 
+-(NSURL*) videoAssetURLToTempFile:(NSURL*)url
+{
+    
+    NSString * surl = [url absoluteString];
+    NSString * ext = [surl substringFromIndex:[surl rangeOfString:@"ext="].location + 4];
+    NSTimeInterval ti = [[NSDate date]timeIntervalSinceReferenceDate];
+    NSString * filename = [NSString stringWithFormat: @"%f.%@",ti,ext];
+    NSString * tmpfile = [NSTemporaryDirectory() stringByAppendingPathComponent:filename];
+
+    ALAssetsLibraryAssetForURLResultBlock resultblock = ^(ALAsset *myasset)
+    {
+        
+        ALAssetRepresentation * rep = [myasset defaultRepresentation];
+        
+        NSUInteger size = [rep size];
+        const int bufferSize = 8192;
+        
+        NSLog(@"Writing to %@",tmpfile);
+        FILE* f = fopen([tmpfile cStringUsingEncoding:1], "wb+");
+        if (f == NULL) {
+            NSLog(@"Can not create tmp file.");
+            return;
+        }
+        
+        Byte * buffer = (Byte*)malloc(bufferSize);
+        int read = 0, offset = 0, written = 0;
+        NSError* err;
+        if (size != 0) {
+            do {
+                read = [rep getBytes:buffer
+                          fromOffset:offset
+                              length:bufferSize
+                               error:&err];
+                written = fwrite(buffer, sizeof(char), read, f);
+                offset += read;
+            } while (read != 0);
+            
+            
+        }
+        fclose(f);                
+    };
+    
+    
+    ALAssetsLibraryAccessFailureBlock failureblock  = ^(NSError *myerror)
+    {
+        NSLog(@"Can not get asset - %@",[myerror localizedDescription]);
+        
+    };
+    
+    if(url)
+    {
+        ALAssetsLibrary* assetslibrary = [[[ALAssetsLibrary alloc] init] autorelease];
+        [assetslibrary assetForURL:url
+                       resultBlock:resultblock
+                      failureBlock:failureblock];
+    }
+    
+    return [NSURL fileURLWithPath:tmpfile];
+}
+
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation
 {
     if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
